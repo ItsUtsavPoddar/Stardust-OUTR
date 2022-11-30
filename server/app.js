c4d5d211705a592d3d1cd7bcd2a6a3c261b57257
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-30 10:47:56 +0530

    initial setup

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const connectToMongo = require('./database');
+const express = require('express');
+var cors = require('cors');
+
+connectToMongo();
+
+// setting up express server
+const app = express();
+const port = 9000;
+
+app.use(cors());
+
+// We use this middle ware to read request body params
+app.use(express.json());
+
+// Avialable Routes
+app.use('/api/auth', require('./routes/auth'));
+//app.use('/api/profile', require('./routes/profile'));
+//app.use('/api/events', require('./routes/events'));
+
+app.listen(port, ()=>{
+    console.log("Server started at port: "+port);
+})
---
+++ server/dev.js
@@ -0,0 +1,24 @@
+const connectToMongo = require('./database');
+const express = require('express');
+var cors = require('cors');
+
+connectToMongo();
+
+// setting up express server
+const app = express();
+const port = 9000;
+
+app.use(cors());
+
+// We use this middle ware to read request body params
+app.use(express.json());
+
+// Avialable Routes
+app.use('/api/dev/doctor', require('./routes/dev/doctor'));
+app.use('/api/dev/hospital', require('./routes/dev/hospital'));
+app.use('/api/dev/medicine', require('./routes/dev/medicine'));
+app.use('/api/dev/medicinecenter', require('./routes/dev/medicinecenter'));
+
+app.listen(port, ()=>{
+    console.log("Server started at port: "+port);
+})
