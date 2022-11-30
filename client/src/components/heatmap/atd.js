2604fdbef6c60a74c3ae506c3cd0865fe0832d26
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-30 16:58:34 +0530

    Update atd.js

---
+++ client/src/components/heatmap/atd.js
@@ -0,0 +1,422 @@
+export const geojson = {
+    "type": "FeatureCollection",
+    "features": [
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.75659494,12.96821717 ]
+     },
+     "properties": {
+     "rating":3.2,
+     "restaurant_name":"Sonu Da Dhaba"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.76178066,12.98185298 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"S.K Fast Food & Tiffin Centre"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.680669,12.912396 ]
+     },
+     "properties": {
+     "rating":2.6,
+     "restaurant_name":"House Boat"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.65278608,12.95461067 ]
+     },
+     "properties": {
+     "rating":4,
+     "restaurant_name":"The Punjabi Bites"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.64584117,13.19146155 ]
+     },
+     "properties": {
+     "rating":3,
+     "restaurant_name":"Shivaji Restaurant"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.61589028,13.1212235 ]
+     },
+     "properties": {
+     "rating":3,
+     "restaurant_name":"Amul Ice Cream Parlour"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.633981,13.019969 ]
+     },
+     "properties": {
+     "rating":3.4,
+     "restaurant_name":"Cart-O-Cafe"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.6801206,12.9130803 ]
+     },
+     "properties": {
+     "rating":3.9,
+     "restaurant_name":"HB Lajawaab"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.49828346,12.9246165 ]
+     },
+     "properties": {
+     "rating":3.7,
+     "restaurant_name":"Biriyani Katte"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.60775615,12.98171609 ]
+     },
+     "properties": {
+     "rating":3.2,
+     "restaurant_name":"Tasty Rides"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.60745104,12.90714155 ]
+     },
+     "properties": {
+     "rating":3.3,
+     "restaurant_name":"Aapikka's Kitchen"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.60203432,12.86640561 ]
+     },
+     "properties": {
+     "rating":3.7,
+     "restaurant_name":"Berry Box Cafe"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.706594,13.198635 ]
+     },
+     "properties": {
+     "rating":2.8,
+     "restaurant_name":"Harvest Market"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.70644501,12.79694653 ]
+     },
+     "properties": {
+     "rating":3.1,
+     "restaurant_name":"Reddy Restaurant"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.50053484,12.96853278 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Bengaluru Tiffin's"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.54649751,12.98474726 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Davanagere Butter Dosa Hotel"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.697354,12.93443578 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Cafe Olive"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.49658428,12.94582277 ]
+     },
+     "properties": {
+     "rating":3.4,
+     "restaurant_name":"Kuteera Park Land"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.59741254,13.04989996 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Chick 'N' Cone"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.63262492,13.03313674 ]
+     },
+     "properties": {
+     "rating":3.5,
+     "restaurant_name":"Silver Spoon Restaurant"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.54571699,12.97720717 ]
+     },
+     "properties": {
+     "rating":3.5,
+     "restaurant_name":"Cafe Bengaluru"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.65724945,12.85175037 ]
+     },
+     "properties": {
+     "rating":3.6,
+     "restaurant_name":"Namma Bengaluru Kitchen"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.49964602,12.91719354 ]
+     },
+     "properties": {
+     "rating":3,
+     "restaurant_name":"Malabar Treat"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.61749726,12.99737271 ]
+     },
+     "properties": {
+     "rating":3.3,
+     "restaurant_name":"Oh My Chaat"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.56516032,12.92429299 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"J Spice"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.671929,12.84808161 ]
+     },
+     "properties": {
+     "rating":2.5,
+     "restaurant_name":"Adarsh Bakery"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.55973119,13.03106878 ]
+     },
+     "properties": {
+     "rating":2.8,
+     "restaurant_name":"Sagar Kabab Corner"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.60229808,12.91654504 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Dine AT Dawn"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.573492,13.027894 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Punjabi Trail"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.64198516,13.0211465 ]
+     },
+     "properties": {
+     "rating":3.7,
+     "restaurant_name":"Dream Scoopers"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.679414,12.8366094 ]
+     },
+     "properties": {
+     "rating":3.7,
+     "restaurant_name":"Tayo Restaurant"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.63515793,13.18911712 ]
+     },
+     "properties": {
+     "rating":3.4,
+     "restaurant_name":"Aviator - Goldfinch Retreat"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.69634649,12.93892326 ]
+     },
+     "properties": {
+     "rating":3.4,
+     "restaurant_name":"Biryani & Kabab Corner"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.71726269,12.96313753 ]
+     },
+     "properties": {
+     "rating":3.5,
+     "restaurant_name":"Chef in"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.70431567,12.96782902 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "restaurant_name":"Tasty Biryani"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.68478081,12.93086874 ]
+     },
+     "properties": {
+     "rating":3,
+     "restaurant_name":"Basaveshwara Khanavali"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.5197237,12.90195282 ]
+     },
+     "properties": {
+     "rating":3.5,
+     "restaurant_name":"Tummies Corner"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.58092131,12.98506253 ]
+     },
+     "properties": {
+     "rating":3.5,
+     "restaurant_name":"Aromas Of South"
+     }
+   }
+]}
