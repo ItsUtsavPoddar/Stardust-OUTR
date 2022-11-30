092b66fa802a35c0a67b6daddff56ddbc95daa92
Utsav Poddar <utsavpoddar99@gmail.com>
2022-11-30 17:34:29 +0530

    heatmap!!!!!!!

--- client/src/components/heatmap/Map.jsx
+++ client/src/components/heatmap/Map.jsx
@@ -1,25 +1,86 @@
-import React from "react";
-import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
-import "../node_modules/leaflet/dist/leaflet.css";
+// import React from "react";
+// import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+// import 'leaflet/dist/leaflet.css';
+// import HeatmapLayer from "react-leaflet-heatmap-layer";
+// import { geojson } from "./atd";
 
-import HeatmapLayer from "react-leaflet-heatmap-layer";
+// class Map extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       lat: 12.9716,
+//       lng: 77.5946,
+//       zoom: 12,
+//       position: [12.9716, 77.5946]
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <LeafletMap center={this.state.position} zoom={this.state.zoom}>
+//         <HeatmapLayer
+//           points={geojson.features}
+//           longitudeExtractor={(m) => m.geometry.coordinates[0]}
+//           latitudeExtractor={(m) => m.geometry.coordinates[1]}
+//           intensityExtractor={(m) => parseFloat(m.geometry.coordinates[1])}
+//           max={100}
+//           minOpacity={0.2}
+//         />
+
+//         <TileLayer
+//           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//           url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+//         />
+//       </LeafletMap>
+//     );
+//   }
+// }
+
+// export default Map;
+
+import React from 'react'
+import './Map.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import HeatmapLayer from "react-leaflet-heatmap-layer-v3/lib/HeatmapLayer";
 import { geojson } from "./atd";
 
-class Map extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      lat: 12.9716,
-      lng: 77.5946,
-      zoom: 12,
-      position: [12.9716, 77.5946]
-    };
-  }
 
-  render() {
-    return (
-      <LeafletMap center={this.state.position} zoom={this.state.zoom}>
-        <HeatmapLayer
+import L from 'leaflet';
+
+let hospitalIcon = L.icon({
+    iconUrl: "https://www.shareicon.net/data/512x512/2016/08/04/806609_medical_512x512.png",
+    iconSize: [25, 25],
+    iconAnchor: [15, 15],
+    
+});
+
+let pharmaIcon = L.icon ({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/8/8115.png",
+  iconSize: [25, 25],
+  iconAnchor: [15, 15],
+})
+
+
+// L.Marker.prototype.options.icon = DefaultIcon;
+
+const Map = () => {
+
+  let hospital = [{x: 20.505, y: 85.10}, {x: 21.0, y: 86.0}]
+  let pharmacy = [{x: 19.505, y: 85.10}, {x: 21.0, y: 85.0}]
+
+  
+
+  return (
+    <>
+    <div className='map-c'>
+    <div className='map d-flex'>
+
+    <MapContainer style={{ width: "60rem", height: "30rem", display: "inline-block", margin: "auto", borderRadius: "10px"}} center={[12.96821717,77.75659494]} zoom={11} scrollWheelZoom={true} >
+
+    <HeatmapLayer
+
           points={geojson.features}
           longitudeExtractor={(m) => m.geometry.coordinates[0]}
           latitudeExtractor={(m) => m.geometry.coordinates[1]}
@@ -28,13 +89,39 @@ class Map extends React.Component {
           minOpacity={0.2}
         />
 
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-      </LeafletMap>
-    );
-  }
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+  />
+
+  
+  {hospital.map((item)=>{
+    return (
+    <Marker position={[item.x, item.y]} icon= {hospitalIcon}>
+      <Popup>
+        Laude Ka Hospital <br />
+      </Popup>
+    </Marker>)
+  })}
+
+  {pharmacy.map((item)=>{
+    return (
+    <Marker position={[item.x, item.y]} icon= {pharmaIcon}>
+      <Popup>
+        Laude Ka Hospital <br />
+      </Popup>
+    </Marker>)
+  })}
+ 
+</MapContainer>
+    </div>
+    {/*<div className="heat-map"><Heat /></div>*/}
+    
+    </div>
+    </>
+  )
+
+
 }
 
-export default Map;
+export default Map
---
+++ client/src/components/polygons/Map.jsx
@@ -0,0 +1,74 @@
+
+import React from 'react'
+import './Map.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+
+import L from 'leaflet';
+
+let hospitalIcon = L.icon({
+    iconUrl: "https://www.shareicon.net/data/512x512/2016/08/04/806609_medical_512x512.png",
+    iconSize: [25, 25],
+    iconAnchor: [15, 15],
+    
+});
+
+let pharmaIcon = L.icon ({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/8/8115.png",
+  iconSize: [25, 25],
+  iconAnchor: [15, 15],
+})
+
+
+// L.Marker.prototype.options.icon = DefaultIcon;
+
+const Map = () => {
+
+  let hospital = [{x: 20.505, y: 85.10}, {x: 21.0, y: 86.0}]
+  let pharmacy = [{x: 19.505, y: 85.10}, {x: 21.0, y: 85.0}]
+
+  
+
+  return (
+    <>
+    <div className='map-c'>
+    <div className='map d-flex'>
+
+    <MapContainer style={{ width: "60rem", height: "30rem", display: "inline-block", margin: "auto", borderRadius: "10px"}} center={[20.5904217383687, 85.86253045523507]} zoom={8} scrollWheelZoom={true} >
+
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+  />
+
+  
+  {hospital.map((item)=>{
+    return (
+    <Marker position={[item.x, item.y]} icon= {hospitalIcon}>
+      <Popup>
+        Laude Ka Hospital <br />
+      </Popup>
+    </Marker>)
+  })}
+
+  {pharmacy.map((item)=>{
+    return (
+    <Marker position={[item.x, item.y]} icon= {pharmaIcon}>
+      <Popup>
+        Laude Ka Hospital <br />
+      </Popup>
+    </Marker>)
+  })}
+ 
+</MapContainer>
+    </div>
+    
+    </div>
+    </>
+  )
+
+
+}
+
+export default Map
