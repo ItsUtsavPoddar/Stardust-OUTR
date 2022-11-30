a9d560bc3a70afc93b46a1a916985652b2ece55b
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-30 15:27:43 +0530

    Create hospitals.js

---
+++ server/routes/public/hospitals.js
@@ -0,0 +1,35 @@
+// Using enviroment variables to save data from being published online
+require('dotenv').config();
+
+const expess = require('express');
+const router = expess.Router();
+const User = require("../../models/User");
+const { body, validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../../middleware/fetchuser');
+const {Auth} = require('two-step-auth');
+const Hospital = require('../../models/Hospital');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Route to get hospital info (doesn't require login)
+router.route('/gethospitals')
+.get(async (req, res)=>{
+    let success = false;
+
+    try{
+        // TODO - Return hospitals based on location
+        /* Use geonear */
+        var hospitals = await Hospital.find({}, 'name email parent_company location pincode size doctors')
+        success = true;
+        return res.json({success, hospitals});
+
+    }catch(error){
+        console.log(error);
+        res.json({success: false, error: 'Something Went Wrong!'});
+    }
+})
+
+
+module.exports = router;
